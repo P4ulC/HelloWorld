3674884a813b10b3bc7eb54a0d5bcbf2e338d567
paul__c <paucal92@gmail.com>
2016-12-12 15:10:15 +0000

    normal

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 
-echo "Hello World...!";
+echo "Hello World...! :)";
 
 //phpinfo();
 
-echo"<p>start</p><hr><pre>";
-
-$homepage = file_get_contents('http://welcome.com.intraorange/');
-echo $homepage;
-
-
-echo"</pre><hr><p>start</p>";
 
 ?>
